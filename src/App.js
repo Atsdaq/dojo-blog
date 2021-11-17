573264e275e861372c5837dcbf3e151ca2766c6c
Atsdaq <eshetuatsdaq@gmail.com>
2021-11-17 11:30:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Home  from './Home';
+function App() {
+  const title= 'Welcome to Jamrock!';
+  return (
+    <div className="App">
+      <Navbar />
+       <div className="content"> 
+       <h1>{title} </h1>
+       <Home/> 
+       </div>
+    </div>
+  );
+}
+
+export default App;
